fc299779a670b912fbd47ac33913174d4191d918
Edvinas Naraveckas <e.naraveckas@gmail.com>
2018-08-27 13:43:30 +0200

    local.dev settings template

---
+++ src/djangoreactredux/settings/local_template.py
@@ -0,0 +1,14 @@
+UNIFIED_DB = {
+    'ENGINE': 'django.db.backends.oracle',
+    'NAME': 'DB_NAME',
+    'USER': 'username',
+    'PASSWORD': 'password',
+}
+
+CA_PATH = '/path/to/CERNRootCertificationAuthority2.crt'
+CERT_PATH = '/path/to/crt.pem'
+CERT_KEY_PATH = '/path/to/key.pem'
+
+REQUEST_MANAGER_API_URL = ''
+SERVER_STATS_API_URL = ''
+
