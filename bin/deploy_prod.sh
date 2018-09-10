32ff00d8118a3966f37ea2ae6322bfec71b782c7
Edvinas Naraveckas <e.naraveckas@gmail.com>
2018-09-10 09:23:13 +0200

    Deployment script

--- bin/deploy_prod.sh
+++ bin/deploy_prod.sh
@@ -4,20 +4,30 @@ export TNS_ADMIN=/eos/project/o/oracle/public/admin/
 # Fetch updates
 git pull origin master
 
-# Set virtual environment
-source wtc-console-env/bin/activate
+# Build frontend for production
+npm run prod
 
-# TODO: Build frontend
+# Stop celery workers
+echo "Stopping Celery workers. If this takes too long, try killing them manually and deleting celeryd.pid\n"
+(cd src \
+    && source wtc-console-env/bin/activate \
+    && celery -A djangoreactredux control shutdown \
+)
+
+# Check if celery workers are stopped
+while [ -f src/celeryd.pid ]
+do
+    echo "."
+    sleep 2
+done
 
 # Install requirement, update static files, start celery, start server
-# TODO change log level to INFO
+# TODO change celery log level to INFO
 (cd src/ \
+    && source wtc-console-env/bin/activate \
     && python manage.py check --deploy \
     && pip install -r py-requirements/prod.txt \
     && python manage.py collectstatic \
     && gunicorn --bind 0.0.0.0:8000 myproject.wsgi:application \
     && celery -A djangoreactredux worker -l debug -f celery.log --detach -B \
 )
-
-# Leave python virtual env
-deactivate
