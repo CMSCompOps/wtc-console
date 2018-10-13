61dcd473577e0a1d06d578e2c03491070ff95dc2
Edvinas Naraveckas <e.naraveckas@gmail.com>
2018-10-13 07:09:23 +0200

    Removing unused code

--- src/static/actions/data.js
+++ src/static/actions/data.js
@@ -1,12 +1,4 @@
 import {
-    FETCH_WORKFLOWS_REQUEST,
-    FETCH_WORKFLOWS_SUCCESS,
-    FETCH_WORKFLOW_REQUEST,
-    FETCH_WORKFLOW_SUCCESS,
-    FETCH_PREP_REQUEST,
-    FETCH_PREP_SUCCESS,
-    FETCH_PREPS_REQUEST,
-    FETCH_PREPS_SUCCESS,
     FETCH_TASKS_REQUEST,
     FETCH_TASKS_SUCCESS,
     FETCH_SITES_REQUEST,
@@ -16,38 +8,6 @@ import {
 } from '../constants';
 import {fetchProtectedData, saveProtectedData} from './api'
 
-
-export function fetchPreps(page, size, filter, orderBy, orderDesc) {
-    return fetchProtectedData(
-        `/api/v1/workflows/preps/?page=${page || 1}&page_size=${size || 20}&filter=${filter || ''}&order_key=${orderBy || ''}&order_desc=${!!orderDesc}`,
-        FETCH_PREPS_REQUEST,
-        FETCH_PREPS_SUCCESS,
-    );
-}
-
-export function fetchPrep(prepId) {
-    return fetchProtectedData(
-        `/api/v1/workflows/preps/${prepId}/`,
-        FETCH_PREP_REQUEST,
-        FETCH_PREP_SUCCESS,
-    );
-}
-
-export function fetchWorkflows(page, size, filter, orderBy, orderDesc) {
-    return fetchProtectedData(
-        `/api/v1/workflows/workflows/?page=${page || 1}&page_size=${size || 20}&filter=${filter || ''}&order_key=${orderBy || ''}&order_desc=${!!orderDesc}`,
-        FETCH_WORKFLOWS_REQUEST,
-        FETCH_WORKFLOWS_SUCCESS,
-    );
-}
-
-export function fetchWorkflow(workflowId) {
-    return fetchProtectedData(
-        `/api/v1/workflows/workflows/${workflowId}/`,
-        FETCH_WORKFLOW_REQUEST,
-        FETCH_WORKFLOW_SUCCESS,
-    );
-}
 
 export function fetchTasks(page, size, filter, orderBy, orderDesc) {
     return fetchProtectedData(
--- src/static/reducers/index.js
+++ src/static/reducers/index.js
@@ -1,18 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import workflowsReducer from './workflows';
-import workflowReducer from './workflow';
-import prepsReducer from './preps';
-import prepReducer from './prep';
 import tasksReducer from './tasks';
 import sitesReducer from './sites';
 
 export default combineReducers({
-    prep: prepReducer,
-    preps: prepsReducer,
     sites: sitesReducer,
-    workflow: workflowReducer,
-    workflows: workflowsReducer,
     tasks: tasksReducer,
     routing: routerReducer,
 });
