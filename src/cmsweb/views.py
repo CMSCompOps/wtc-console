359fd2f7c0f4e13421fd6ca7f3fbbfd6cf25ee2d
Andres Vargas <vargasa@cua.edu>
2019-01-04 19:44:35 +0100

    Workflow recovery info, sites included

--- src/cmsweb/views.py
+++ src/cmsweb/views.py
@@ -2,8 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+
+from cmstoolbox.workflowinfo import WorkflowInfo
 
 
 def workflow_sites(request, workflow_id):
-    return HttpResponse("Workflow id: {}".format(workflow_id))
+
+    wf = WorkflowInfo( workflow = workflow_id)
+
+    return JsonResponse(wf.get_recovery_info())
