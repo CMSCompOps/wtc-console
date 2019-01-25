8abcca0c24f9de33ea73eb6db1c4ff8f9631a67e
Andres Vargas <vargasa@cua.edu>
2019-01-25 23:58:48 +0100

    Add 'misc' app:     'misc' will be the place to store requests to 'external' APIs which      can not be processed by the client either due to CORS restrictions      or due to authorization requirements

--- src/djangoreactredux/settings/base.py
+++ src/djangoreactredux/settings/base.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = (
     'base',
     'unified',
     'workflows',
+
+    'misc',
 )
 
 MIDDLEWARE_CLASSES = (
--- src/djangoreactredux/urls.py
+++ src/djangoreactredux/urls.py
@@ -6,6 +6,7 @@ from base import views as base_views
 
 urlpatterns = [
     url(r'^api/v1/workflows/', include('workflows.urls', namespace='workflows')),
+    url(r'^api/v1/misc/', include('misc.urls', namespace='misc')),
 
     # catch all others because of how history is handled by react router - cache this page because it will never change
     url(r'', cache_page(settings.PAGE_CACHE_SECONDS)(base_views.IndexView.as_view()), name='index'),
