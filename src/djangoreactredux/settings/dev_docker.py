fc68f7060df2ba0eb39ebafdc1caaf055d4f2618
dabercro <dabercro@mit.edu>
2019-04-02 17:08:01 -0400

    Get docker-compose working out of the box

--- src/djangoreactredux/settings/dev_docker.py
+++ src/djangoreactredux/settings/dev_docker.py
@@ -1,26 +1,63 @@
 import mongoengine
 
-from djangoreactredux.settings.dev import *  # NOQA (ignore all errors on this line)
-from djangoreactredux.settings.local import *
+from djangoreactredux.settings.base import *
+
+DEBUG = True
+
+PAGE_CACHE_SECONDS = 1
 
 DATABASES = {
     'default': {
         'ENGINE': '',
     },
-    'unified': UNIFIED_DB,
-}
-
-MONGODB_DATABASES = {
-    'default': {
-        'NAME': 'wtc-console',
-        'HOST': 'mongo',
-        'PORT': 8081,
-        'USER': 'root',
-        'PASSWORD': 'root',
+    'unified': {
+        'ENGINE': '',
     }
 }
 
 mongoengine.connect(
-    db=MONGODB_DATABASES['default']['NAME'],
-    host=MONGODB_DATABASES['default']['HOST']
+    db='wtc-console',
+    host='mongo'
 )
+
+REST_FRAMEWORK['EXCEPTION_HANDLER'] = 'django_rest_logger.handlers.rest_exception_handler'  # NOQA (ignore all errors on this line)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['django_rest_logger_handler'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'django_rest_logger_handler': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['django_rest_logger_handler'],
+            'propagate': False,
+        },
+        'django_rest_logger': {
+            'level': 'DEBUG',
+            'handlers': ['django_rest_logger_handler'],
+            'propagate': False,
+        },
+    },
+}
+
+DEFAULT_LOGGER = 'django_rest_logger'
+
+LOGGER_EXCEPTION = DEFAULT_LOGGER
+LOGGER_ERROR = DEFAULT_LOGGER
+LOGGER_WARNING = DEFAULT_LOGGER
